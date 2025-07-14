66fe49e27fa7a69a6982d59b260f23560515fc06
Caleb! <48127194+kaf-lamed-beyt@users.noreply.github.com>
2025-07-14 10:17:57 +0100

    fix: fixed a bug where db writes were performed on every snapshot (#8)
    this is bad, the db operation should only be called when the cached data is different from the new one from the most recent snapshot.
    
    make db no go blow;

--- src/lib/cron/snapshot.ts
+++ src/lib/cron/snapshot.ts
@@ -8,6 +8,25 @@ export async function takeSnapshot() {
 
   for (const user of users) {
     const newSnapshot = await fetchGitHubFollowersForUser(user, GITHUB_PAT!);
+    const prevSnapshots = await db().getRecentSnapshotsForUser(user.githubId);
+    const latestSnapshot = prevSnapshots[0] ?? [];
+
+    const prevUsernames = new Set(latestSnapshot.map((f) => f.username));
+    const newUsernames = new Set(newSnapshot.map((f) => f.username));
+
+    const isSameLength = prevUsernames.size === newUsernames.size;
+    const isSameSet =
+      isSameLength &&
+      [...newUsernames].every((username) => prevUsernames.has(username));
+
+    if (isSameSet) {
+      console.log(
+        `No change in snapshot for ${user.username}. Skipping insert.`
+      );
+      continue;
+    }
+
     await db().addFollowers(newSnapshot, user.githubId);
+    console.log(`New snapshot inserted for ${user.username}`);
   }
 }
