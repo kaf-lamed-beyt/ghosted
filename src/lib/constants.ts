8bf5ec1f675a1a8d977d78b084936c0c8d56cef7
Caleb! <48127194+kaf-lamed-beyt@users.noreply.github.com>
2025-09-07 22:59:28 +0100

    chore: send welcome email to new users (#14)
    * chore: send welcome email to new users
    
    thank you *[yerrie](https://github.com/chinyere-nwalie)* for suggesting this.
    
    * only send to new users
    
    * format
    
    * appease knip

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -19,3 +19,22 @@ export const skeleton = {
   startColor: 'var(--color-dark-charcoal)',
   endColor: 'var(--color-alt-white)',
 };
+
+export const emailSection = {
+  padding: '24px 20px',
+  fontFamily: 'Helvetica, Arial, sans-serif',
+  backgroundColor: '#f9f9f9',
+};
+
+export const emailContainer = {
+  backgroundColor: '#fff',
+  padding: '24px',
+  borderRadius: '8px',
+  boxShadow: '0 1px 3px rgba(0,0,0,0.05)',
+};
+
+export const emailHeading = {
+  fontWeight: '400',
+  fontSize: '20px',
+  marginBottom: '12px',
+};
